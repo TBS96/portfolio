5215dbaeb467ae48f1ae892aa123494850330855
TBS96 <9tbs6@proton.me>
2025-02-26 21:00:05 +0530

    rename reducer name from themeReducer to themeSlice
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import themeReducer from './themeSlice';
+import themeSlice from './themeSlice';
 
 const store = configureStore({
     reducer: {
-        theme: themeReducer,
+        theme: themeSlice,
     },
 });
 
