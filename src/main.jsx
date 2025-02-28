76633f1f7beb293e57d4d05ea479cf20506dc138
TBS96 <9tbs6@proton.me>
2025-02-28 21:43:58 +0530

    create page Home.jsx...
    render it through main.jsx as child element of App.jsx(basically renders through <Outlet />)
    
    deleted unnecessary import for StrictMode in main.jsx
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import Home from './pages/Home.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        // path: '/',
+        index: true,  // recommended for "Home", when using nested routes
+        element: <Home />
+      }
+    ],
   }
 ]);
 
