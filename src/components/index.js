358fd87d0292e6d97e2d357732cb0f61acf2dacf
TBS96 <9tbs6@proton.me>
2025-03-01 23:14:46 +0530

    create component AboutMe.jsx
    import it in Home page
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,12 @@ import Container from "./Container/Container";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Hero from "./Hero";
+import AboutMe from "./AboutMe";
+
 export {
     Navbar,
     Container,
     Footer,
     Hero,
+    AboutMe,
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Hero } from '../components'
+import { AboutMe, Hero } from '../components'
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <AboutMe />
         </>
     )
 }
