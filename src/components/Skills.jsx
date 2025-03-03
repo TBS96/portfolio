23fa53e19e9d87407e8b7c03e68ddf2aef843147
TBS96 <9tbs6@proton.me>
2025-03-04 00:02:06 +0530

    refactor Skills component...
    replace hoverText with url for skill links
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { url } from 'react-router-dom';
 
 const Skills = () => {
 
@@ -9,113 +9,133 @@ const Skills = () => {
         {
             name: 'HTML5',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
-            hoverText: 'https://www.w3schools.com/html/'
+            url: 'https://www.w3schools.com/html/'
         },
         {
             name: 'CSS3',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
-            hoverText: 'https://www.w3schools.com/css/'
+            url: 'https://www.w3schools.com/css/'
         },
         {
             name: 'JavaScript',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
-            hoverText: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+            url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
         },
         {
             name: 'JSON',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/json/json-original.svg',
-            hoverText: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON'
+            url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON'
         },
         {
             name: 'React.js',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
-            hoverText: 'https://react.dev/reference/react'
+            url: 'https://react.dev/reference/react'
+        },
+        {
+            name: 'Vite',
+            icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg',
+            url: 'https://vite.dev/'
         },
         {
             name: 'Redux',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg',
-            hoverText: 'https://redux-toolkit.js.org/introduction/getting-started'
+            url: 'https://redux-toolkit.js.org/introduction/getting-started'
         },
         {
             name: 'Tailwind CSS',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg',
-            hoverText: 'https://tailwindcss.com/docs/installation/using-vite'
+            url: 'https://tailwindcss.com/docs/installation/using-vite'
         },
         {
             name: 'Material UI',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg',
-            hoverText: 'https://mui.com/material-ui/getting-started/'
+            url: 'https://mui.com/material-ui/getting-started/'
         },
         {
             name: 'Bootstrap',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
-            hoverText: 'https://getbootstrap.com/docs/5.3/getting-started/introduction/'
+            url: 'https://getbootstrap.com/docs/5.3/getting-started/introduction/'
         },
         {
             name: 'Firebase',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg',
-            hoverText: 'https://firebase.google.com/docs/auth?_gl=1*lrtmgh*_up*MQ..&gclid=CjwKCAiAw5W-BhAhEiwApv4goN-nFN9bqVqiajGOM1qCp-AgTZ_0Bd0AAVSzt-IkrtayG3uf_M8j9xoCli4QAvD_BwE&gclsrc=aw.ds'
+            url: 'https://firebase.google.com/docs/auth?_gl=1*lrtmgh*_up*MQ..&gclid=CjwKCAiAw5W-BhAhEiwApv4goN-nFN9bqVqiajGOM1qCp-AgTZ_0Bd0AAVSzt-IkrtayG3uf_M8j9xoCli4QAvD_BwE&gclsrc=aw.ds'
         },
         {
             name: 'appwrite',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/appwrite/appwrite-original.svg',
-            hoverText: 'https://appwrite.io/docs'
+            url: 'https://appwrite.io/docs'
         },
         {
             name: 'Git',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
-            hoverText: 'https://git-scm.com/'
+            url: 'https://git-scm.com/'
         },
         {
             name: 'GitHub',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
-            hoverText: 'https://github.com/tbs96'
+            url: 'https://github.com/tbs96'
+        },
+        {
+            name: 'GitHub Codespaces',
+            icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/githubcodespaces/githubcodespaces-original.svg',
+            url: 'https://github.com/features/codespaces'
         },
         {
             name: 'GitLab',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg',
-            hoverText: 'https://gitlab.com/TBS96'
+            url: 'https://gitlab.com/TBS96'
+        },
+        {
+            name: 'Markdown',
+            icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/markdown/markdown-original.svg',
+            url: 'https://www.markdownguide.org/getting-started/'
         },
         {
             name: 'Vercel',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vercel/vercel-original.svg',
-            hoverText: 'https://vercel.com/prantik-ghoshs-projects'
+            url: 'https://vercel.com/prantik-ghoshs-projects'
         },
         {
             name: 'Netlify',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/netlify/netlify-original.svg',
-            hoverText: 'https://app.netlify.com/teams/prantikg96/sites'
+            url: 'https://app.netlify.com/teams/prantikg96/sites'
         },
         {
             name: 'npm',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg',
-            hoverText: 'https://www.npmjs.com/'
+            url: 'https://www.npmjs.com/'
         },
         {
             name: 'VS Code',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg',
-            hoverText: 'https://code.visualstudio.com/'
+            url: 'https://code.visualstudio.com/'
         },
         {
             name: 'Node.js',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg',
-            hoverText: 'https://nodejs.org/docs/latest/api/'
+            url: 'https://nodejs.org/docs/latest/api/'
         },
         {
             name: 'Nodemon',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodemon/nodemon-original.svg',
-            hoverText: 'https://nodemon.io/'
+            url: 'https://nodemon.io/'
         },
         {
             name: 'Express.js',
             icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg',
-            hoverText: 'https://expressjs.com/'
+            url: 'https://expressjs.com/'
+        },
+        {
+            name: 'Linux',
+            icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg',
+            url: 'https://www.linuxmint.com/'
         },
     ];
 
     const handleSkillClick = (index) => {
         setActiveSkill(activeSkill === index ? null : index);
-        console.log(activeSkill);
+        // console.log(activeSkill);   // 1st click=null, 2nd click=index
     };
 
     return (
@@ -127,7 +147,7 @@ const Skills = () => {
 
                 {/* <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 justify-center items-center'> */}
                 <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 justify-center items-center'>
-                    {skills.map(({ name, icon, hoverText }, index) => (
+                    {skills.map(({ name, icon, url }, index) => (
                         <div 
                             key={index} 
                             onClick={() => handleSkillClick(index)} 
@@ -140,9 +160,9 @@ const Skills = () => {
 
                             {activeSkill === index && (
                                 <div className='absolute inset-0 flex justify-center items-center text-xl font-bold text-white'>
-                                    <Link to={hoverText} target='_blank'>
-                                        Visit: <span className='text-error'>{hoverText.replace(hoverText, name)}</span>
-                                    </Link>
+                                    <url to={url} target='_blank'>
+                                        Visit: <span className='text-error'>{url.replace(url, name)}</span>
+                                    </url>
                                 </div>
                             )}
 
