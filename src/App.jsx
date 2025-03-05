e3efcec57208f76d2d29f3be02b4947d5164268f
TBS96 <9tbs6@proton.me>
2025-03-05 18:30:13 +0530

    feat: add loading indicator to App component during initial load
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Footer, Navbar } from './components'
 import { Outlet } from 'react-router-dom'
+import { LifeLine } from 'react-loading-indicators';
 
 const App = () => {
-  return (
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return !loading ? (
     <div className='min-h-screen bg-gradient-to-br from-red-300 to-blue-500'>
-      <div>
-        <Navbar />
-        <Container>
-          <main>
-            <Outlet />
-          </main>
-        </Container>
-        <Footer />
-      </div>
+      <Navbar />
+      <Container>
+        <main>
+          <Outlet />
+        </main>
+      </Container>
+      <Footer />
+    </div>
+  ) : (
+    <div className='grid place-content-center w-full min-h-screen'>
+      <LifeLine easing='ease-in' speedPlus={2} text='Welcome to my Portfolio' size='large' />
     </div>
   )
 }
