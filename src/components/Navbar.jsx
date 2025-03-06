50c804e32fe210d1284222708af8182788d66cbb
TBS96 <9tbs6@proton.me>
2025-03-06 21:02:07 +0530

    feat: expand theme options in Navbar and enhance dropdown styling for better usability
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
-const themes = ['light', 'dark', 'retro', 'cyberpunk', 'valentine', 'aqua'];
+const themes = ['light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'haloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter', 'dim', 'nord', 'sunset', 'caramellatte', 'abyss', 'silk'];
 
 const Navbar = () => {
 
@@ -43,7 +43,10 @@ const Navbar = () => {
                 {/* Theme Dropdown (Mobile & Tablet) */}
                 <div className='dropdown dropdown-center md:hidden'>
                     <div tabIndex={0} role='button' className='btn btn-ghost'>
-                        Theme
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                        </svg>
+
                         <svg
                             width='12px'
                             height='12px'
@@ -54,7 +57,7 @@ const Navbar = () => {
                             <path d='M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z'></path>
                         </svg>
                     </div>
-                    <ul tabIndex={0} className='dropdown-content bg-base-300/90 rounded-box p-2 shadow-2xl'>
+                    <ul tabIndex={0} className='dropdown-content h-60 overflow-auto bg-base-300/90 rounded-box p-2 shadow-2xl'>
                         {themes.map((t) => (
                             <li key={t}>
                                 <input
@@ -89,7 +92,10 @@ const Navbar = () => {
                 {/* Desktop Theme Dropdown (Right Side) */}
                 <div className='dropdown dropdown-center hidden md:block'>
                     <div tabIndex={0} role='button' className='btn btn-ghost'>
-                        Theme
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                        </svg>
+
                         <svg
                             width='12px'
                             height='12px'
@@ -100,7 +106,7 @@ const Navbar = () => {
                             <path d='M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z'></path>
                         </svg>
                     </div>
-                    <ul tabIndex={0} className='dropdown-content bg-base-300/90 rounded-box w-40 p-2 shadow-2xl'>
+                    <ul tabIndex={0} className='dropdown-content h-60 overflow-auto bg-base-300/90 rounded-box w-40 p-2 shadow-2xl'>
                         {themes.map((t) => (
                             <li key={t}>
                                 <input
