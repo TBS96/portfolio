757676ea90b13de8dbf28ab7a6d1ea3fa42fe333
TBS96 <9tbs6@proton.me>
2025-04-16 22:12:40 +0530

    feat: implement loading state with BlinkBlur animation in App component
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { Container, Footer, Navbar } from './components'
 import { Outlet } from 'react-router-dom'
-import { LifeLine } from 'react-loading-indicators';
+import { BlinkBlur } from 'react-loading-indicators';
 
 const App = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className='grid place-content-center w-full min-h-screen'>
+        <BlinkBlur easing='ease-in' speedPlus={1} text='Welcome to my Portfolio' size='large' />
+      </div>
+    )
+  }
 
   return (
     <Suspense fallback={
       <div className='grid place-content-center w-full min-h-screen'>
-        <LifeLine easing='ease-in' speedPlus={1} text='Welcome to my Portfolio' size='large' />
+        <BlinkBlur easing='ease-in' speedPlus={1} text='Welcome to my Portfolio' size='large' />
       </div>
     }>
       <div className='min-h-screen'>
