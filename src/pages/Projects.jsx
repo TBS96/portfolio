e4c7f73d0a3ed0221b210fae987b48b3390ed42d
TBS96 <9tbs6@proton.me>
2025-04-24 00:13:59 +0530

    fix: revert back to document title in useEffect for proper SEO handling
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,9 +3,12 @@ import { Projects as ProjectsPage } from '../components'
 
 const Projects = () => {
 
+    useEffect(() => {
+        document.title = 'Prantik Ghosh | Projects';
+    }, []);
+
     return (
         <>
-            <title>Prantik Ghosh | Projects</title>
             <ProjectsPage />
         </>
     )
